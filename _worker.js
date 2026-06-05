a2c1fff0f0f82b37d4a0ac97e74bd5ea6a4060e8
KP-CHANNEL-KP <kopaing232003@gmail.com>
2026-06-05 18:03:44 +0630

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ export default {
     const url = new URL(request.url);
     
     // ဒီမှာ သင် လိုချင်တဲ့ လမ်းကြောင်းအလိုက် Redirect လုပ်နိုင်ပါတယ်
-    if (url.pathname === "/KPCHANNEL") {
-      return Response.redirect("https://www.kpkey.mytunnel.org/sub?token=bb4898b2d58cd1d794790e866aa915a7", 301);
+    if (url.pathname === "/KPCHANNEL346") {
+      return Response.redirect("https://www.kpkey.mytunnel.org/sub?token=6d7bddae7a6c9b731dcd306836c22674", 301);
     }
     
     return new Response("Welcome to my shortener!");
